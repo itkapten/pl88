38db001b057a6156bdccbdc5f493e6ffc9121d6e
itkapten <itkapten69@gmail.com>
2026-03-01 04:29:22 +0700

    Update dewa.js

--- js/dewa.js
+++ js/dewa.js
@@ -1,6 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+
     var el = document.querySelector(".custom-page");
     if (el) {
         el.remove();
     }
+
+    var images = document.querySelectorAll("img:not([loading])");
+    images.forEach(function (img) {
+        img.setAttribute("loading", "lazy");
+    });
+
+    if (window.location.hostname === "app.planet-88.org") {
+        var style = document.createElement("style");
+        style.innerHTML = ".downloadapk { display: none !important; }";
+        document.head.appendChild(style);
+    }
+
 });
